2d4ce7892a2fbaf2ab7cf99b94f9a98f2828576c
jallorenzo <jallorenzo7@gmail.com>
2017-08-25 03:26:57 +0800

    Cash register layout

--- POSwithAndroid/Forms/POS.Designer.vb
+++ POSwithAndroid/Forms/POS.Designer.vb
@@ -22,7 +22,7 @@ Partial Class POS
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.listViewInvoicce = New System.Windows.Forms.ListView()
         Me.txtBxProductSearch = New System.Windows.Forms.TextBox()
         Me.lblProductId = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
@@ -32,22 +32,30 @@ Partial Class POS
         Me.ClosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.StocksToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtboxProdName = New System.Windows.Forms.TextBox()
+        Me.txtboxQuantity = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblChange = New System.Windows.Forms.Label()
+        Me.lblAReceived = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
         Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'ListView1
+        'listViewInvoicce
         '
-        Me.ListView1.Location = New System.Drawing.Point(12, 79)
-        Me.ListView1.Name = "ListView1"
-        Me.ListView1.Size = New System.Drawing.Size(707, 318)
-        Me.ListView1.TabIndex = 2
-        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.listViewInvoicce.Location = New System.Drawing.Point(12, 79)
+        Me.listViewInvoicce.Name = "listViewInvoicce"
+        Me.listViewInvoicce.Size = New System.Drawing.Size(707, 318)
+        Me.listViewInvoicce.TabIndex = 2
+        Me.listViewInvoicce.UseCompatibleStateImageBehavior = False
         '
         'txtBxProductSearch
         '
         Me.txtBxProductSearch.Location = New System.Drawing.Point(739, 99)
         Me.txtBxProductSearch.Name = "txtBxProductSearch"
-        Me.txtBxProductSearch.Size = New System.Drawing.Size(369, 22)
+        Me.txtBxProductSearch.Size = New System.Drawing.Size(322, 22)
         Me.txtBxProductSearch.TabIndex = 3
         '
         'lblProductId
@@ -55,23 +63,23 @@ Partial Class POS
         Me.lblProductId.AutoSize = True
         Me.lblProductId.Location = New System.Drawing.Point(736, 79)
         Me.lblProductId.Name = "lblProductId"
-        Me.lblProductId.Size = New System.Drawing.Size(74, 17)
+        Me.lblProductId.Size = New System.Drawing.Size(65, 17)
         Me.lblProductId.TabIndex = 4
-        Me.lblProductId.Text = "Product ID"
+        Me.lblProductId.Text = "Barcode:"
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(467, 400)
+        Me.Label1.Location = New System.Drawing.Point(476, 410)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(109, 17)
+        Me.Label1.Size = New System.Drawing.Size(137, 17)
         Me.Label1.TabIndex = 5
-        Me.Label1.Text = "Total Amount: ₱"
+        Me.Label1.Text = "Total Amount:        ₱"
         '
         'lblTotalAmount
         '
         Me.lblTotalAmount.AutoSize = True
-        Me.lblTotalAmount.Location = New System.Drawing.Point(607, 400)
+        Me.lblTotalAmount.Location = New System.Drawing.Point(616, 410)
         Me.lblTotalAmount.Name = "lblTotalAmount"
         Me.lblTotalAmount.Size = New System.Drawing.Size(44, 17)
         Me.lblTotalAmount.TabIndex = 6
@@ -111,17 +119,93 @@ Partial Class POS
         Me.StocksToolStripMenuItem.Size = New System.Drawing.Size(63, 24)
         Me.StocksToolStripMenuItem.Text = "&Stocks"
         '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(736, 152)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(102, 17)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Product Name:"
+        '
+        'txtboxProdName
+        '
+        Me.txtboxProdName.Location = New System.Drawing.Point(739, 172)
+        Me.txtboxProdName.Name = "txtboxProdName"
+        Me.txtboxProdName.Size = New System.Drawing.Size(322, 22)
+        Me.txtboxProdName.TabIndex = 8
+        '
+        'txtboxQuantity
+        '
+        Me.txtboxQuantity.Location = New System.Drawing.Point(961, 252)
+        Me.txtboxQuantity.Name = "txtboxQuantity"
+        Me.txtboxQuantity.Size = New System.Drawing.Size(100, 22)
+        Me.txtboxQuantity.TabIndex = 10
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(890, 255)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(65, 17)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Quantity:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(476, 485)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(134, 17)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "Change:                ₱"
+        '
+        'lblChange
+        '
+        Me.lblChange.AutoSize = True
+        Me.lblChange.Location = New System.Drawing.Point(613, 485)
+        Me.lblChange.Name = "lblChange"
+        Me.lblChange.Size = New System.Drawing.Size(44, 17)
+        Me.lblChange.TabIndex = 13
+        Me.lblChange.Text = "00.00"
+        '
+        'lblAReceived
+        '
+        Me.lblAReceived.AutoSize = True
+        Me.lblAReceived.Location = New System.Drawing.Point(613, 440)
+        Me.lblAReceived.Name = "lblAReceived"
+        Me.lblAReceived.Size = New System.Drawing.Size(44, 17)
+        Me.lblAReceived.TabIndex = 15
+        Me.lblAReceived.Text = "00.00"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(476, 440)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(136, 17)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "Amount Received: ₱"
+        '
         'POS
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1120, 595)
+        Me.Controls.Add(Me.lblAReceived)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.lblChange)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtboxQuantity)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtboxProdName)
         Me.Controls.Add(Me.MenuStrip1)
         Me.Controls.Add(Me.lblTotalAmount)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.lblProductId)
         Me.Controls.Add(Me.txtBxProductSearch)
-        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.listViewInvoicce)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Name = "POS"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
@@ -132,7 +216,7 @@ Partial Class POS
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents listViewInvoicce As System.Windows.Forms.ListView
     Friend WithEvents txtBxProductSearch As System.Windows.Forms.TextBox
     Friend WithEvents lblProductId As System.Windows.Forms.Label
     Friend WithEvents Label1 As System.Windows.Forms.Label
@@ -142,5 +226,13 @@ Partial Class POS
     Friend WithEvents ClosToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
     Friend WithEvents StocksToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtboxProdName As System.Windows.Forms.TextBox
+    Friend WithEvents txtboxQuantity As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents lblChange As System.Windows.Forms.Label
+    Friend WithEvents lblAReceived As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
 
 End Class
